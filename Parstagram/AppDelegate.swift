a78808c93a48f98c577b0a6ce800b8fcbcf4215d
IkennaAku <ikennaokenwa2004@gmail.com>
2022-03-16 16:03:45 -0500

    app was able to comment

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -11,8 +11,8 @@ import Parse
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    
+  
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
         let parseConfig = ParseClientConfiguration {
@@ -21,6 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     $0.server = "https://parseapi.back4app.com"
             }
             Parse.initialize(with: parseConfig)
+        
+      
             
         return true
     }
